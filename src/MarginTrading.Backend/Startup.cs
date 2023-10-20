e2b778d31678b1c92ce56703ca3ac9cfbfdfd526
atarutin <andrey.tarutin@lykke-business.ch>
2023-10-20 16:28:59 +0300

    fix: add rabbitmq connection retries

--- src/MarginTrading.Backend/Startup.cs
+++ src/MarginTrading.Backend/Startup.cs
@@ -12,6 +12,8 @@ using Lykke.Cqrs;
 using Lykke.Logs.MsSql;
 using Lykke.Logs.MsSql.Repositories;
 using Lykke.Logs.Serilog;
+using Lykke.Messaging.RabbitMq;
+using Lykke.Messaging.RabbitMq.Retry;
 using Lykke.SettingsReader;
 using Lykke.Snow.Common.Correlation;
 using Lykke.Snow.Common.Correlation.Cqrs;
@@ -92,10 +94,7 @@ namespace MarginTrading.Backend
 
             services.AddSingleton(Configuration);
             services
-                .AddControllers(opt =>
-                {
-                    opt.ModelBinderProviders.Insert(0, new CoreModelBinderProvider());
-                })
+                .AddControllers(opt => { opt.ModelBinderProviders.Insert(0, new CoreModelBinderProvider()); })
                 .AddNewtonsoftJson(options =>
                 {
                     options.SerializerSettings.ContractResolver = new DefaultContractResolver();
@@ -109,7 +108,7 @@ namespace MarginTrading.Backend
             {
                 options.SwaggerDoc("v1",
                     new OpenApiInfo
-                    { Title = $"MarginTradingEngine_Api_{Configuration.ServerType()}", Version = "v1" });
+                        { Title = $"MarginTradingEngine_Api_{Configuration.ServerType()}", Version = "v1" });
                 options.AddApiKeyAwareness();
             }).AddSwaggerGenNewtonsoftSupport();
 
@@ -122,6 +121,15 @@ namespace MarginTrading.Backend
                 });
 
             services.AddFeatureManagement(_mtSettingsManager.CurrentValue.MtBackend);
+
+            services.Configure<RabbitMqRetryPolicyOptions>(opt =>
+            {
+                opt.InitialConnectionSleepIntervals = _mtSettingsManager.CurrentValue.MtBackend.RabbitMqRetryPolicy
+                    .InitialConnectionSleepIntervals;
+                opt.RegularSleepIntervals =
+                    _mtSettingsManager.CurrentValue.MtBackend.RabbitMqRetryPolicy.RegularSleepIntervals;
+            });
+            services.AddRabbitMqMessaging();
 
             SetupLoggers(Configuration, services, _mtSettingsManager, correlationContextAccessor);
         }
@@ -237,7 +245,7 @@ namespace MarginTrading.Backend
             builder.RegisterModule(new MarginTradingCommonModule());
             builder.RegisterModule(new ExternalServicesModule(mtSettings));
             builder.RegisterModule(new BackendMigrationsModule());
-            builder.RegisterModule(new CqrsModule(settings.CurrentValue.Cqrs, settings.CurrentValue, LogLocator.CommonLog));
+            builder.RegisterModule(new CqrsModule(settings.CurrentValue.Cqrs, settings.CurrentValue));
 
             builder.RegisterBuildCallback(c =>
             {
