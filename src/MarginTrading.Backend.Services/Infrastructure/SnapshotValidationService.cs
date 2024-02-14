78c2a9cb7cbef69bc010171517e573aa19c61278
Aliaksandr Vashetsin <aliaksandr.vashetsin@lykke-business.ch>
2024-02-14 12:06:55 +0300

    temp log

--- src/MarginTrading.Backend.Services/Infrastructure/SnapshotValidationService.cs
+++ src/MarginTrading.Backend.Services/Infrastructure/SnapshotValidationService.cs
@@ -74,6 +74,9 @@ namespace MarginTrading.Backend.Services.Infrastructure
             var ordersValidationResult = CompareOrders(currentOrders, restoredOrders);
             var positionsValidationResult = ComparePositions(currentPositions, restoredPositions);
 
+            await _log.WriteInfoAsync(nameof(SnapshotValidationService), nameof(ValidateCurrentStateAsync),
+                $"Temp log: currentOrders=${currentOrders.Length}, restoredOrders=${restoredOrders.Count}, latestLastModified=${latestLastModified}");
+            
             if (ordersValidationResult.IsValid)
             {
                 await _log.WriteInfoAsync(nameof(SnapshotValidationService), nameof(ValidateCurrentStateAsync),
