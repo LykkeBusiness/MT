62d7a5b5cf29bd14a6596b234187099e73246472
Aliaksandr Vashetsin <aliaksandr.vashetsin@lykke-business.ch>
2024-03-21 10:29:35 +0100

    LT-4926: suggested changes

--- src/MarginTrading.Backend.Services/Infrastructure/SnapshotValidationService.cs
+++ src/MarginTrading.Backend.Services/Infrastructure/SnapshotValidationService.cs
@@ -62,10 +62,8 @@ namespace MarginTrading.Backend.Services.Infrastructure
             var lastOrders = GetOrders(tradingEngineSnapshot);
             var lastPositions = GetPositions(tradingEngineSnapshot);
 
-            var latestLastModified = currentOrders.Any()
-                ? currentOrders.Max(x => x.LastModified)
-                : (DateTime?)null;
-            var ordersHistory = await _ordersHistoryRepository.GetLastSnapshot(tradingEngineSnapshot.Timestamp, latestLastModified);
+            var latestOrder = currentOrders.MaxBy(x => x.LastModified);
+            var ordersHistory = await _ordersHistoryRepository.GetLastSnapshot(tradingEngineSnapshot.Timestamp, latestOrder?.LastModified);
             var positionsHistory = await _positionsHistoryRepository.GetLastSnapshot(tradingEngineSnapshot.Timestamp);
 
             var restoredOrders = RestoreOrdersCurrentStateFromHistory(lastOrders, ordersHistory);
