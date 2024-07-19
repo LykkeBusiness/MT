eefc148c291a7aa03da3c3509f6147e6eda1c294
Gennadii Ponomarev <gennady.ponomarev@lykke-business.ch>
2024-07-19 10:50:24 +0500

    fix(LT-5621): add retry policy on redis calls

--- src/MarginTrading.Backend.Services/Services/SnapshotTrackerService.cs
+++ src/MarginTrading.Backend.Services/Services/SnapshotTrackerService.cs
@@ -1,9 +1,12 @@
 // Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Threading.Tasks;
-using MarginTrading.Backend.Core;
 using MarginTrading.Backend.Core.Services;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
 using StackExchange.Redis;
 
 namespace MarginTrading.Backend.Services.Services
@@ -11,24 +14,30 @@ namespace MarginTrading.Backend.Services.Services
     public class SnapshotTrackerService : ISnapshotTrackerService
     {
         private readonly IDatabase _database;
+        private readonly AsyncRetryPolicy _retryPolicy;
         
         private const string RedisKey = "core:snapshot:should-recreate";
         
-        public SnapshotTrackerService(IConnectionMultiplexer redis)
+        public SnapshotTrackerService(IConnectionMultiplexer redis, ILogger<SnapshotTrackerService> logger)
         {
             _database = redis.GetDatabase();
+            _retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(3,
+                    x => TimeSpan.FromMilliseconds(x * 1000),
+                    (exception, span) => logger.LogWarning("Exception: {Message}", exception?.Message));
         }
 
         public async Task SetShouldRecreateSnapshot(bool value)
         {
-            await _database
-                .StringSetAsync(RedisKey, value.ToString(), when: When.Always);
+            await _retryPolicy.ExecuteAsync(() => _database
+                .StringSetAsync(RedisKey, value.ToString(), when: When.Always)); 
         }
         
         public async Task<bool> GetShouldRecreateSnapshot()
         {
-            var value = await _database
-                .StringGetAsync(RedisKey);
+            var value = await _retryPolicy.ExecuteAsync(() => 
+              _database.StringGetAsync(RedisKey));
 
             return value == bool.TrueString;
         }
