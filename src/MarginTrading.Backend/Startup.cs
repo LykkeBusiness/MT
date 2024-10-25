14574f0c0af15fdf72564ae84064eec017ef93a7
atarutin <andrey.tarutin@lykke-business.ch>
2024-10-25 10:46:19 +0300

    chore: Update Lykke.MarginTrading.OrderBookService.Contracts to version 2.0.0

--- src/MarginTrading.Backend/Startup.cs
+++ src/MarginTrading.Backend/Startup.cs
@@ -200,9 +200,9 @@ namespace MarginTrading.Backend
                         .Resolve<IConfigurationValidator>()
                         .WarnIfInvalidAsync();
 
-                    ApplicationContainer
+                    await ApplicationContainer
                         .Resolve<IClientProfileSettingsCache>()
-                        .Start();
+                        .StartAsync();
 
                     ApplicationContainer
                         .Resolve<ICqrsEngine>()
