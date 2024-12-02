88229bd25a90e43f67378763f8aeeea955fad9b6
Michael Kobzev <69039160+kobzev@users.noreply.github.com>
2024-12-02 16:06:13 +0300

    fix(LT-5626): update the logic of generator

--- src/MarginTrading.Backend.Services/AlphanumericIdentityGenerator.cs
+++ src/MarginTrading.Backend.Services/AlphanumericIdentityGenerator.cs
@@ -4,38 +4,23 @@
 using System;
 using System.Linq;
 
-using Serilog;
-
 namespace MarginTrading.Backend.Services;
 public static class AlphanumericIdentityGenerator
 {
-    // we must not generate id's starting with these keywords, it causes situations like `POSPOS` in reporting. Valid situations, but mind-blowing.
-    private static readonly string[] RestrictedBeginnings = ["POS", "TRD", "ORD", "CLS", "KOM", "DIV", "UEW", "TCC"];
-    private const string DefaultPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static readonly Func<string, bool>[] DefaultRestrictions = [x => RestrictedBeginnings.Any(x.StartsWith)];
-
+    // we must not generate id's starting with PTOCKDU letters
+    // It causes situations like `POSPOS` in reporting. Valid situations, but mind-blowing.
+    private const string FirstLetterPool = "ABEFGHIJLMNQRSVWXYZ0123456789";
+    private const string DefaultPool     = "ABEFGHIJLMNQRSVWXYZ0123456789" + "PTOCKDU";
     private static readonly Random Random = new();
     private static readonly object LockObject = new();
-    private static readonly ILogger Log = Serilog.Log.ForContext(typeof(AlphanumericIdentityGenerator));
 
-    public static string Generate(string pool = DefaultPool, Func<string, bool>[] restrictions = null, int length = 10)
+    public static string Generate(string pool = DefaultPool, string firstLetterPool = FirstLetterPool, int length = 10)
     {
         lock(LockObject)
         {
-            var restrictionsToApply = restrictions ?? DefaultRestrictions;
-            string resultingString;
-            var attempts = 0;
-            do
-            {
-                resultingString = new string(Enumerable.Range(0, length).Select(_ => pool[Random.Next(0, pool.Length)]).ToArray());
-
-                if (attempts++ > 100)
-                {
-                    Log.Warning("Restriction validation failed 100 times in a row. Check calling code, restrictions are almost impossible to be met.");
-                    break;
-                }
-            } while (restrictionsToApply.Any(x => x.Invoke(resultingString)));
-            return resultingString;
+            char firstLetter = firstLetterPool[Random.Next(0, firstLetterPool.Length)];
+            string randomTail = new(Enumerable.Range(0, length - 1).Select(_ => pool[Random.Next(0, pool.Length)]).ToArray());
+            return firstLetter + randomTail;
         }
     }
 }
