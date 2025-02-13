37043fb6e5ae38882ac2eba4d05bbac636b28146
atarutin <142044+tarurar@users.noreply.github.com>
2025-02-13 18:57:44 +0300

    refactor: simplify SnapshotMonitorSettings by removing unused properties

--- src/MarginTrading.Backend.Core/Settings/SnapshotMonitorSettings.cs
+++ src/MarginTrading.Backend.Core/Settings/SnapshotMonitorSettings.cs
@@ -3,28 +3,14 @@
 
 using System;
 
-namespace MarginTrading.Backend.Core.Settings
+namespace MarginTrading.Backend.Core.Settings;
+
+public class SnapshotMonitorSettings
 {
-    public class SnapshotMonitorSettings
-    {
-        private const int DefaultSnapshotCreationAtempts = 10;
-        private const int DefaultMonitoringDelayInSeconds = 30;
-        private const int DefaultDelayBeforeFallbackSnapshotInMinutes = 5;
+    private const int DefaultMonitoringDelayInSeconds = 30;
 
-        /// <summary>
-        /// Defines the interval between consecutive checks performed by the SnapshotMonitoringService service.
-        /// </summary>
-        public TimeSpan MonitoringDelay { get; set; } = TimeSpan.FromSeconds(DefaultMonitoringDelayInSeconds);
-
-        /// <summary>
-        /// Maximum number of attempts to create a snapshot CONSISTENTLY
-        /// </summary>
-        public ushort MaxSnapshotCreationAttempts { get; set; } = DefaultSnapshotCreationAtempts;
-
-        /// <summary>
-        /// If snapshot is not created after a specified amount of time, creation will be retried
-        /// </summary>
-        public TimeSpan DelayBeforeFallbackSnapshot { get; set; } =
-            TimeSpan.FromMinutes(DefaultDelayBeforeFallbackSnapshotInMinutes);
-    }
+    /// <summary>
+    /// Defines the interval between consecutive checks performed by the SnapshotMonitoringService service.
+    /// </summary>
+    public TimeSpan MonitoringDelay { get; set; } = TimeSpan.FromSeconds(DefaultMonitoringDelayInSeconds);
 }
